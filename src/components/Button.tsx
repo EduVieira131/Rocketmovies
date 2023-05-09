e25ecfedc93d6666d9dc0b5968333d6c4d287cb2
EduViera131 <eduviera369@gmail.com>
2023-05-09 09:54:49 -0300

    Create movie page sctructure

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,9 +7,9 @@ interface ButtonProps {
 
 export function Button(props: ButtonProps) {
   return (
-    <button className="flex items-center justify-center gap-2 w-full max-w-[90px] lg:max-w-[210px] bg-[#FF859B] rounded-lg px-8 py-3 text-[#1C1B1E]">
+    <button className="flex items-center justify-center gap-2 w-full bg-[#FF859B] rounded-lg px-8 py-3 text-[#1C1B1E] font-medium">
       {props.children}
-      <p className="hidden lg:flex"> {props.title} </p>
+      {props.title}
     </button>
   )
 }
--- src/pages/CreateMovie.tsx
+++ src/pages/CreateMovie.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../components/Button'
 import { ButtonText } from '../components/ButtonText'
 import { Header } from '../components/Header'
 import { Input } from '../components/Input'
@@ -7,14 +8,39 @@ export function CreateMovie() {
     <>
       <Header />
 
-      <main>
-        <ButtonText placeholder="Voltar" haveIcon />
+      <main className="max-w-6xl mx-auto p-4 flex flex-col gap-10">
+        <div>
+          <ButtonText placeholder="Voltar" haveIcon />
+          <h1 className="text-[#F4EDE8] text-4xl font-medium mt-6">
+            Novo Filme
+          </h1>
+        </div>
 
-        <h1>Novo Filme</h1>
+        <div className="flex items-center gap-10">
+          <Input placeholder="Título" />
+          <Input placeholder="Sua nota de 0 a 5" type="number" />
+        </div>
+
+        <textarea
+          id="observations"
+          placeholder="Observações"
+          className="w-full h-72 bg-[#262529] focus:outline-none py-4 px-6 rounded-xl text-[#F4EDE8] resize-none"
+        ></textarea>
 
         <div>
-          <Input placeholder='Título'/>
-          <Input placeholder='Sua nota de 0 a 5' type='number'/>
+          <h2 className="text-[#999591] text-xl mb-6 font-normal">
+            Marcadores
+          </h2>
+
+          <div className="bg-[#0D0C0F] rounded-lg w-full p-4"></div>
+        </div>
+
+        <div className='flex gap-10'>
+          <div className="bg-[#0D0C0F] rounded-lg py-4 px-8 flex items-center justify-center w-full">
+            <ButtonText placeholder="Excluir filme" />
+          </div>
+
+          <Button title="Salvar alterações" />
         </div>
       </main>
     </>
