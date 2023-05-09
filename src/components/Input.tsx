37117d3531cd12a5107d89088689265a521a1365
EduViera131 <eduviera369@gmail.com>
2023-05-09 09:18:28 -0300

    Component input type functionality added

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,15 @@
 interface InputProps {
   placeholder: string
   icon?: any
+  type?: string
 }
 
-export function Input({ placeholder, icon: Icon, ...rest }: InputProps) {
+export function Input({ placeholder, icon: Icon, type,...rest }: InputProps) {
   return (
     <div className="flex items-center gap-3 bg-[#262529] w-full py-4 px-6 rounded-xl text-[#F4EDE8]">
       {Icon !== undefined ? <Icon className="text-[#948F99]" /> : ''}
       <input
-        type="text"
+        type={type ? type : 'text'}
         placeholder={placeholder}
         className="bg-[#262529] w-full h-full focus:outline-none"
         {...rest}
--- src/pages/CreateMovie.tsx
+++ src/pages/CreateMovie.tsx
@@ -1,11 +1,22 @@
+import { ButtonText } from '../components/ButtonText'
 import { Header } from '../components/Header'
+import { Input } from '../components/Input'
 
 export function CreateMovie() {
   return (
     <>
       <Header />
 
-      
+      <main>
+        <ButtonText placeholder="Voltar" haveIcon />
+
+        <h1>Novo Filme</h1>
+
+        <div>
+          <Input placeholder='Título'/>
+          <Input placeholder='Sua nota de 0 a 5' type='number'/>
+        </div>
+      </main>
     </>
   )
 }
