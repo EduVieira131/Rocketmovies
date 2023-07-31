454d0ae8292810da569f5bea2d58351b9f689035
EduViera131 <eduviera369@gmail.com>
2023-07-31 10:18:11 -0300

    :fix: removed responsiviness from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 export function Header() {
   return (
-    <header className="flex items-center justify-center flex-wrap lg:flex-nowrap gap-3 lg:gap-16 w-full px-4 lg:px-28 py-6 border-b-2 border-[#3E3B47] ">
+    <header className="flex items-center justify-center gap-16 w-full px-28 py-6 border-b-2 border-[#3E3B47] ">
       <h1 className="text-[#FF859B] text-lg lg:text-2xl font-bold place-self-center">
         RocketMovies
       </h1>
