ea671b40256950af9edc7f6e1ad674004e8c324d
EduViera131 <eduviera369@gmail.com>
2023-08-03 16:17:18 -0300

    :refac: refactored tag component

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,10 +2,10 @@ interface TagProps {
   label: string
 }
 
-export function Tag(props: TagProps) {
+export function Tag({ label }: TagProps) {
   return (
-    <span className=" flex items-center w-fit bg-[#312E38] px-4 py-1 text-[#E5E5E5] text-sm rounded-lg">
-      {props.label}
+    <span className=" flex w-fit items-center rounded-lg bg-[#312E38] px-4 py-1 text-sm text-[#E5E5E5]">
+      {label}
     </span>
   )
 }
