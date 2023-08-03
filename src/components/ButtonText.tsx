6c9e03d88fadcb3fb21ab3a78563a13c511c28d1
EduViera131 <eduviera369@gmail.com>
2023-08-03 16:20:49 -0300

    :refac: refactored button text component

--- src/components/ButtonText.tsx
+++ src/components/ButtonText.tsx
@@ -1,19 +1,21 @@
 import { FiArrowLeft } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
 
 interface ButtonTextProps {
   placeholder: string
-  to: string
   haveIcon?: boolean
 }
 
-export function ButtonText(props: ButtonTextProps) {
+export function ButtonText({
+  placeholder,
+  haveIcon,
+  ...rest
+}: ButtonTextProps) {
   return (
-    <Link
-      to={props.to}
-      className="flex items-center justify-center gap-2 w-fit text-[#FF859B]"
+    <button
+      className="flex w-fit items-center justify-center gap-2 text-[#FF859B]"
+      {...rest}
     >
-      {props.haveIcon ? <FiArrowLeft /> : ''} {props.placeholder}
-    </Link>
+      {haveIcon ? <FiArrowLeft /> : ''} {placeholder}
+    </button>
   )
 }
